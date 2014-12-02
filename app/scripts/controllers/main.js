4f3122440300c2fe29cd8a8a3be98ff6ba7ebfb7
Jason Dinkelmann <jason@aboutgolf.com>
2014-12-01 22:31:38 -0500

    add basic todo functionality

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,4 +11,19 @@ todoApp
   .controller('MainCtrl', function ($scope) {
         $scope.welcome = "Welcome to the Brighton Area Software Society";
         $scope.introduction = "Introduction to AngularJS - Jason Dinkelmann";
+
+        $scope.taskForm = {};
+        $scope.taskList = [];
+
+
+        $scope.addTask = function() {
+            // get the task from the model
+            var title = $scope.taskForm.title;
+            var date = $scope.taskForm.dueDate;
+
+            $scope.taskList.push({
+                title: title,
+                dueDate: date
+            });
+        }
   });
